4ccc70a11fb2effbfae282c34e3eb1b9d6a3b59f
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-21 13:22:25 +0200

    Add handlers, mocks and server setuptest to test useUser

---
+++ src/mocks/user/userMocks.ts
@@ -0,0 +1,8 @@
+import { type UserCredentials } from "../../types";
+
+export const tokenMock = "tokenMock";
+
+export const userCredentialsMock: UserCredentials = {
+  username: "carlos",
+  password: "carlos",
+};
