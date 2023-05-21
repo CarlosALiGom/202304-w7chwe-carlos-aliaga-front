e8e0a536c9c326405294e9f0a8b1cea6b766f000
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-21 18:57:51 +0200

    Test Header component

---
+++ src/components/Header/Header.test.tsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+describe("Given a Header component", () => {
+  describe("When the component is rendered", () => {
+    test("Then it should show the logo", () => {
+      const expectedAlternativeText = "!Facebook logo";
+      const routes = [
+        {
+          path: "/",
+          element: <Header />,
+        },
+      ];
+
+      const router = createBrowserRouter(routes);
+
+      render(<RouterProvider router={router} />);
+
+      const logo = screen.getByAltText(expectedAlternativeText);
+
+      expect(logo).toBeInTheDocument();
+    });
+  });
+});
