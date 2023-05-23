dd0375a960e65fbffc4571eb963ae7539c45e615
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-23 06:40:55 +0200

    Add ContactsPage and route on appRouter

---
+++ src/pages/ContactsPage/ContactsPage.tsx
@@ -0,0 +1,9 @@
+const ContactsPage = (): JSX.Element => {
+  return (
+    <>
+      <h2 className="contacts-title">!Frenemies</h2>
+    </>
+  );
+};
+
+export default ContactsPage;
--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,13 +1,16 @@
-import LoginForm from "../components/LoginForm/LoginForm";
-import useUser from "../hooks/useUser/useUser";
-import { UserCredentials } from "../types";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import useUser from "../../hooks/useUser/useUser";
+import { UserCredentials } from "../../types";
 
 const LoginPage = (): JSX.Element => {
   const { getUserToken } = useUser();
 
   const handleFormSubmit = async (user: UserCredentials) => {
     const token = await getUserToken(user);
-    window.localStorage.setItem("token", token);
+
+    if (token) {
+      window.localStorage.setItem("token", token);
+    }
   };
 
   return <LoginForm submitForm={handleFormSubmit} />;
