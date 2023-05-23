9c0659fd210c28f37d5b5fe18a3896c6ef7d82af
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-23 07:23:01 +0200

    Add useToken to LoginPage and mocks to test custom hook

---
+++ src/mocks/mocks.ts
@@ -0,0 +1,9 @@
+export const realTokenMock =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDY4ZmFhODcwOTA2NjNmZjIxMmQ2OGEiLCJuYW1lIjoiY2FybG9zIiwiaWF0IjoxNjg0NzM2MTkzLCJleHAiOjE2ODQ4MjI1OTN9.dn2Eq2cc8GauRf6qiJnuI6sTeE17dchp2GvxAjsr7Gk";
+
+export const userTokenDataMock = {
+  id: "6468faa87090663ff212d68a",
+  name: "carlos",
+  token:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI2NDY4ZmFhODcwOTA2NjNmZjIxMmQ2OGEiLCJuYW1lIjoiY2FybG9zIiwiaWF0IjoxNjg0NzM2MTkzLCJleHAiOjE2ODQ4MjI1OTN9.dn2Eq2cc8GauRf6qiJnuI6sTeE17dchp2GvxAjsr7Gk",
+};
