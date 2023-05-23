7262e00a5e8c73b69819a48f10ef70a9c915a441
Carlos Aliaga <carlosaligom@gmai.com>
2023-05-23 07:53:56 +0200

    Add renderWithProviders and refactor pages tests

--- src/pages/ContactsPage/ContactsPage.test.tsx
+++ src/pages/ContactsPage/ContactsPage.test.tsx
@@ -1,12 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import ContactsPage from "./ContactsPage";
+import renderWithProviders from "../../utils/testUtils";
 
 describe("Given a ContactsPage component", () => {
   describe("When it is render", () => {
     test("Then it should show a heading with '!Frenemies'", () => {
       const text = "!Frenemies";
 
-      render(<ContactsPage />);
+      renderWithProviders(<ContactsPage />);
 
       const heading = screen.getByRole("heading", { name: text, level: 2 });
 
--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,12 +1,13 @@
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import LoginPage from "./LoginPage";
+import renderWithProviders from "../../utils/testUtils";
 
 describe("Given a LoginFormPage component", () => {
   describe("When it is render", () => {
     test("Then it should show a heading with 'Login'", () => {
       const text = "Login";
 
-      render(<LoginPage />);
+      renderWithProviders(<LoginPage />);
 
       const heading = screen.getByRole("heading", { name: text, level: 2 });
 
